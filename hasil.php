d93e23eec2a49acfcbeff1888419d36c56df678a
Nadthasa <nadyagrumps11@gmail.com>
2024-06-20 01:13:06 +0700

    Add files via upload

---
+++ hasil.php
@@ -0,0 +1,7 @@
+<?php
+echo "<center>nama lengkap: ".$_POST['namalengkap']."</center><br>";
+echo "<center>email: ".$_POST['email']."</center><br>";
+echo "<center>phone: ".$_POST['phone']."</center><br>";
+echo "<center>birthdate: ".$_POST['birthdate']."</center><br>";
+echo "<center>radio: ".$_POST['radio']."</center><br>";
+?>
